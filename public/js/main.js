c3de95db4a432e310130c332056be19992fd9693
MohannedElfatih <gailardiagalane@gmail.com>
2017-04-02 18:10:27 +0400

    Adjusted the chat interface UI.

--- public/js/main.js
+++ public/js/main.js
@@ -11,7 +11,7 @@ var config = {
 };
 firebase.initializeApp(config);
 
-app1.config(function($routeProvider, $locationProvider) {
+app1.config(function ($routeProvider, $locationProvider) {
     $locationProvider.hashPrefix('');
     $routeProvider
         .when('/', {
@@ -19,7 +19,7 @@ app1.config(function($routeProvider, $locationProvider) {
             controller: 'main'
         })
         .when('/main', {
-            templateUrl: './views/main.html',
+            templateUrl: './views/main2.html',
             controller: 'Test'
         })
         .otherwise({
@@ -27,12 +27,12 @@ app1.config(function($routeProvider, $locationProvider) {
         });
 });
 
-app1.directive('messagesData', ['$location', '$anchorScroll', function($location, $anchorScroll) {
+/*app1.directive('messagesData', ['$location', '$anchorScroll', function($location, $anchorScroll) {
     return {
         scope: {
             message: "=",
         },
-        template: '<p><span>{{message.name}}: </span>{{message.message}}</p>',
+        template: '<p><span class="testing">{{message.name}}: </span>{{message.message}}</p>',
         restrict: 'E',
         compile: function(element, attrs) {
             console.log("compiler");
@@ -42,9 +42,26 @@ app1.directive('messagesData', ['$location', '$anchorScroll', function($location
             document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
         }
     };
+}]);*/
+
+app1.directive('messagesData', ['$location', '$anchorScroll', function ($location, $anchorScroll) {
+    return {
+        scope: {
+            message: "=",
+        },
+        templateUrl: './views/messageData.html',
+        restrict: 'EA',
+        compile: function (element, attrs) {
+            console.log("compiler");
+            document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
+        },
+        link: function (element, attrs) {
+            document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
+        }
+    };
 }]);
 
-app1.directive('notifyUser', function() {
+app1.directive('notifyUser', function () {
     return {
         scope: false,
         template: '<p id="notifyUser">{{notifyUser}}</p>',
@@ -52,7 +69,23 @@ app1.directive('notifyUser', function() {
     }
 });
 
-app1.controller('main', function($scope, $rootScope, $firebaseObject) {
+app1.directive('leftChatMenu', function () {
+    return {
+        scope: false,
+        templateUrl: './views/leftMenu.html',
+        restrict: 'E'
+    }
+});
+
+app1.directive('rightChat', function () {
+    return {
+        scope: false,
+        templateUrl: './views/rightChat.html',
+        restrict: 'E'
+    }
+});
+
+app1.controller('main', function ($scope, $rootScope, $firebaseObject) {
     $scope.glued = true;
     $scope.notifyUser = "Welcome! Enter credentials to Sign up.";
     const rootRef = firebase.database().ref();
@@ -65,7 +98,7 @@ app1.controller('main', function($scope, $rootScope, $firebaseObject) {
         console.log(snap.numChildren());
         var counter = 0;
         $scope.messages = new Array();
-        snap.forEach(function(childSnapshot) {
+        snap.forEach(function (childSnapshot) {
             console.log(childSnapshot.child('message').val());
             console.log(childSnapshot.child('name').val());
             $scope.messages.push({
@@ -75,22 +108,22 @@ app1.controller('main', function($scope, $rootScope, $firebaseObject) {
         });
     });
 
-    $scope.scrolldown = function() {
+    $scope.scrolldown = function () {
         console.log("scrolling");
         document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
     };
 
-    $scope.signOut = function(){
-      console.log("Am in signout function");
-      firebase.auth().signOut()
-      .then(function(){
-        console.log("Signed out");
-      }).catch(function(error){
-          console.log(error.message);
-      });
+    $scope.signOut = function () {
+        console.log("Am in signout function");
+        firebase.auth().signOut()
+            .then(function () {
+                console.log("Signed out");
+            }).catch(function (error) {
+                console.log(error.message);
+            });
     }
 
-    $scope.submit = function() {
+    $scope.send = function () {
         var pushMessage = rootRef.child("messages").push();
         pushMessage.set({
             "message": $scope.textBox,
@@ -98,14 +131,12 @@ app1.controller('main', function($scope, $rootScope, $firebaseObject) {
         });
 
         $scope.textBox = '';
-
-        $scope.$on('ngRepeatFinished', function(ngRepeatFinishedEvent) {
-            document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
-        });
+        
+        document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;
     };
 });
 
-app1.controller('Test', function($location, $scope, $rootScope, $firebaseObject, $timeout) {
+app1.controller('Test', function ($location, $scope, $rootScope, $firebaseObject, $timeout) {
     const rootRef = firebase.database().ref();
     var object = $firebaseObject(rootRef);
     var provider = new firebase.auth.FacebookAuthProvider();
@@ -116,9 +147,9 @@ app1.controller('Test', function($location, $scope, $rootScope, $firebaseObject,
     const connectFB = document.getElementById('connectFB');
     var signUp = document.getElementById('signUp');
 
-    $scope.signUp = function(e) {
+    $scope.signUp = function (e) {
         $scope.notifyUser = "Signing Up...";
-        var signUpTimeout = $timeout(function() {
+        var signUpTimeout = $timeout(function () {
             $scope.notifyUser = "An error happened, check credentials.";
         }, 5000);
         const auth = firebase.auth();
@@ -136,9 +167,9 @@ app1.controller('Test', function($location, $scope, $rootScope, $firebaseObject,
             });
     };
 
-    $scope.logIn = function(e) {
+    $scope.logIn = function (e) {
         $scope.notifyUser = "Logging in...";
-        var logInTimeOut = $timeout(function() {
+        var logInTimeOut = $timeout(function () {
             $scope.notifyUser = "An error happened, check credentials.";
         }, 3000);
         const promise = firebase.auth().signInWithEmailAndPassword(document.getElementById("username").value,
@@ -153,22 +184,22 @@ app1.controller('Test', function($location, $scope, $rootScope, $firebaseObject,
         console.log($scope.notifyUser);
     };
 
-    $scope.connectFB = function(e) {
+    $scope.connectFB = function (e) {
         $scope.notifyUser = "Logging in...";
         var provider = new firebase.auth.FacebookAuthProvider();
         const promise = firebase.auth().signInWithPopup(provider);
         promise.then(result => {
-                var token = result.credential.accessToken;
-                var user = result.user;
-                $location.path("/main");
-                $scope.$apply();
-                console.log(user);
-            }).catch(e => {
-                console.log("error code is " + e.code);
-                console.log("error message is " + e.message);
-                console.log("error email is " + e.email);
-                console.log("error credential is " + e.credential);
-            });
+            var token = result.credential.accessToken;
+            var user = result.user;
+            $location.path("/main");
+            $scope.$apply();
+            console.log(user);
+        }).catch(e => {
+            console.log("error code is " + e.code);
+            console.log("error message is " + e.message);
+            console.log("error email is " + e.email);
+            console.log("error credential is " + e.credential);
+        });
     };
 
     firebase.auth().onAuthStateChanged(firebaseUser => {
